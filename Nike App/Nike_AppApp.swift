2f115e712319112deeb9e35ee2f6a6b951b0fe49
maxim <gmv.vrn@@bk.ru>
2025-11-20 19:26:17 +0300

    Initial Commit

---
+++ Nike App/Nike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nike_AppApp.swift
+//  Nike App
+//
+//  Created by goncharenkom on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Nike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
